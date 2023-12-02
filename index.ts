4def1623cf7c1c81f89ba988cfdf25a5f1eb0636
MisileLab <misileminecord@gmail.com>
2023-12-02 18:43:50 +0900

    some comment
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const router = new Router();
 const db = edgedb.createClient();
 let message = {};
 
+// 실시간 채팅을 위한 웹소켓
 router.get("/ws", (ctx) => {
   if (!ctx.isUpgradable) {
     ctx.response.status = 400;
@@ -13,6 +14,7 @@ router.get("/ws", (ctx) => {
   }
   const ws = ctx.upgrade();
   let _original = {}
+  // 채팅 데이터가 바뀔 경우 바뀐 데이터 전송
   setInterval(()=>{
     if (message == _original) {return;}
     if (ws.readyState != 1) {return;}
@@ -22,18 +24,21 @@ router.get("/ws", (ctx) => {
   }, 1000);
 })
 
+// 유저 정보 전송
 router.get("/users", async (ctx) => {
   const users = await db.queryJSON("select Account { username, accid, description, image, manage: {name, elements: {name, type, value}}}");
   ctx.response.status = 200;
   ctx.response.body = users;
 })
 
+// 회원가입 요청
 router.post("/register", async (ctx) => {
   const {headers} = ctx.request;
   const username = headers.get("username");
   const accid = headers.get("accid");
   const password = headers.get("password");
   const description = headers.get("description");
+  // 만약 accid가 이미 존재한다면 409 Conflict
   if (await db.querySingle("select Account { username, accid, description, image, manage } filter .accid = <str>$accid", {accid}) != null) {
     ctx.response.status = 409;
     ctx.response.body = "accid already exists";
@@ -43,10 +48,12 @@ router.post("/register", async (ctx) => {
   await db.querySingle("insert Account { username := <str>$username, accid := <str>$accid, password := <str>$password, description := <str>$description}", {username, accid, password, description});
 })
 
+// 로그인 요청
 router.get("/login", async (ctx) => {
   const {headers} = ctx.request;
   const accid = headers.get("accid");
   const password = headers.get("password");
+  // accid와 password가 일치하는 유저가 없다면 401 Unauthorized
   const user = await db.querySingle("select Account { username, accid, description, image, manage } filter .accid = <str>$accid and .password = <str>$password", {accid, password});
   if (user == null) {
     ctx.response.status = 401;
@@ -56,11 +63,13 @@ router.get("/login", async (ctx) => {
   ctx.response.body = user;
 })
 
+// 메시지 리스트 요청
 router.get("/msgs", async (ctx) => {
   const msg = await db.queryJSON("select Message {from, to, text, time}");
   ctx.response.body = msg;
 })
 
+// 메시지 요청
 router.get("/msg", async (ctx) => {
   const {headers} = ctx.request;
   const from = headers.get("from");
@@ -69,6 +78,7 @@ router.get("/msg", async (ctx) => {
   ctx.response.body = msg;
 })
 
+// 메시지 전송 요청
 router.post("/msg", async (ctx) => {
   const headerlist = ctx.request.headers;
   const from = headerlist.get("from");
@@ -76,6 +86,7 @@ router.post("/msg", async (ctx) => {
   const content = headerlist.get("content");
   const time = Date.now();
   const json = await db.querySingleJSON("insert Message {from := <str>$from, to := <str>$to, text := <str>$content, time := <int64>$time}", {from, to, content, time});
+  // 메시지 변경 전송
   message = {
     "id": JSON.parse(json)["id"],
     "from": from,
@@ -87,6 +98,7 @@ router.post("/msg", async (ctx) => {
   ctx.response.status = 201;
 })
 
+// 설명 변경
 router.post("/update_desc", async (ctx) => {
   const {headers} = ctx.request;
   const accid = headers.get("accid");
@@ -95,14 +107,15 @@ router.post("/update_desc", async (ctx) => {
   ctx.response.status = 200;
 });
 
+// 일정 변경
 router.post("/update_manage", async (ctx) => {
   const {headers} = ctx.request;
   const accid = headers.get("accid");
   const manage = headers.get("manage");
   if (manage === null) {ctx.response.status = 400; return;}
   const managev = JSON.parse(manage);
-  const {name} = managev;
   console.log(managev);
+  // 다 지우고 새로 데이터 연동
   const manages = JSON.parse(await db.queryJSON("select Manage {name}filter .parent_accid = <str>$accid", {accid}));
   for (const i of manages) {
     await db.query("delete ManageElement filter .parent_name = <str>$name", {name: i.name});
